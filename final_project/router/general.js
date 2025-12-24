063810eb71c0691fb2591613d8532071a9758858
Mohamed Hamed <mhmd.h.hamed@gmail.com>
2025-12-24 18:41:17 +0100

    Task 2 and 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,19 +11,34 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  return res.status(300).send(JSON.stringify(books, null, 4));
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (isbn in books) {
+    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+  }
+  return res
+    .status(400)
+    .json({ message: `Book with isbn ${isbn} does not exist` });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+
+  const results = Object.entries(books)
+    .filter(([, b]) => b.author === author)
+    .map(([isbn, b]) => ({ isbn: isbn, title: b.title, reviews: b.reviews }));
+
+  if (results.length >= 1) {
+    return res.status(200).send(JSON.stringify(results, null, 4));
+  }
+  return res
+    .status(400)
+    .json({ message: `Author ${req.params.author} does not exist` });
 });
 
 // Get all books based on title
